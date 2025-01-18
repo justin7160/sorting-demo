82450674bad90e3e3468d71edae1a877299eeb28
justinchen <justinchen@universalec.com>
2025-01-18 23:55:57 +0800

    init project2

---
+++ README.md
@@ -0,0 +1,31 @@
+# Sorting Demo(Visualizer)
+
+Sorting Demo(Visualizer) is a web application that provides a visual representation of different sorting algorithms. This project references and incorporates code from [Clement Mihailescu's Sorting Visualizer Tutorial](https://github.com/clementmihailescu/Sorting-Visualizer-Tutorial) and has been adapted for Node.js version 18.
+
+---
+
+## Features
+
+- Visualization of popular sorting algorithms:
+    - Merge Sort
+
+---
+
+## Prerequisites
+
+- **Node.js**: Version 18 or higher
+- **npm**: Comes with Node.js installation
+
+---
+
+## Technologies Used
+
+- **Frontend**: React.js
+- **Styling**: CSS
+
+---
+
+## Acknowledgments
+
+- [Clement Mihailescu](https://github.com/clementmihailescu) for the original tutorial and inspiration.
+
---
+++ src/SortingVisualizer/SortingVisualizer.jsx
@@ -0,0 +1,85 @@
+import React from "react";
+import {getMergeSortAnimations} from "../SortingAlgorithms/sortingAlgorithms.js";
+import "./SortingVisualizer.css";
+
+const NUMBER_OF_ARRAY_BARS = 100;
+
+const PRIMARY_COLOR = 'turquoise';
+
+const SECONDARY_COLOR = 'red';
+
+const ANIMATION_SPEED_MS = 8;
+
+export default class SortingVisualizer extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            array: []
+        };
+    }
+
+    componentDidMount() {
+        this.resetArray();
+    }
+
+    resetArray() {
+        const array = [];
+        for (let i = 0; i < NUMBER_OF_ARRAY_BARS; i++) {
+            array.push(randomIntFromInterval(10, 800));
+        }
+        //ES6 {} is used to define the object which its key and props are the same
+        this.setState({array});
+    }
+
+    mergeSort() {
+        const animations = getMergeSortAnimations(this.state.array);
+        for (let i = 0; i < animations.length; i++) {
+            const arrayBars = document.getElementsByClassName("array-bar");
+            const isColorChange = i % 3 !== 2;
+            if (isColorChange) {
+                const [barOneIdx, barTwoIdx] = animations[i];
+                const barOneStyle = arrayBars[barOneIdx].style;
+                const barTwoStyle = arrayBars[barTwoIdx].style;
+                const color = i % 3 === 0 ? SECONDARY_COLOR : PRIMARY_COLOR;
+                setTimeout(() => {
+                    barOneStyle.backgroundColor = color;
+                    barTwoStyle.backgroundColor = color;
+                }, i * ANIMATION_SPEED_MS);
+            } else {
+                setTimeout(() => {
+                    const [barOneIdx, newHeight] = animations[i];
+                    const barOneStyle = arrayBars[barOneIdx].style;
+                    barOneStyle.height = `${newHeight}px`;
+                }, i * ANIMATION_SPEED_MS);
+            }
+        }
+    }
+
+    render() {
+        const {array} = this.state;
+
+        return (
+            <div className="array-container">
+                {array.map((value, index) => (
+                    <div
+                        className="array-bar"
+                        key={index}
+                        style={{
+                            backgroundColor: PRIMARY_COLOR,
+                            height: `${value}px`
+                        }}
+                    ><span>{value}</span></div>
+                ))}
+                <button onClick={() => this.resetArray()}>重新生成陣列</button>
+                <button onClick={() => this.mergeSort()}>Merge Sort</button>
+            </div>
+        );
+    }
+
+}
+
+function randomIntFromInterval(min, max) {
+    console.log("randomIntFromInterval");
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
